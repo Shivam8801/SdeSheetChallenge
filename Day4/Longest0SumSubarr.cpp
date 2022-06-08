1d29d17690b53ada5c053aff57106d74cf732734
Shivam Nikam <shivammh15@gmail.com>
2022-06-08 14:21:47 +0530

    Create Longest0SumSubarr.cpp

---
+++ Day4/Longest0SumSubarr.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h> 
+int LongestSubsetWithZeroSum(vector < int > arr) {
+    
+    unordered_map<int, int> mp;
+    
+    int sum = 0;
+    int len = 0;
+    for(int i  = 0; i < arr.size(); i++)
+    {
+        sum += arr[i];
+        
+        if(sum == 0)
+            len = i + 1;
+        else
+        {
+            if(mp.find(sum) != mp.end())
+            len = max(len, i - mp[sum]);
+            
+            else mp[sum] = i;
+        }
+    }
+    return len;
+}
