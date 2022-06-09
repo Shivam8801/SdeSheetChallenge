640b10125a2ce6705aee61113f10fc03e607bad8
Shivam Nikam <shivammh15@gmail.com>
2022-06-09 22:13:59 +0530

    Create DeleteAGivenNode.cpp

---
+++ Day5/DeleteAGivenNode.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h> 
+/****************************************************************
+
+    Following is the class structure of the LinkedListNode class:
+
+    template <typename T>
+    class LinkedListNode
+    {
+    public:
+        T data;
+        LinkedListNode<T> *next;
+        LinkedListNode(T data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+*****************************************************************/
+
+void deleteNode(LinkedListNode<int> * node) {
+    while(node->next->next != NULL)
+    {
+        node->data = node->next->data;
+        node = node->next;
+    }
+    node->data = node->next->data;
+    node->next =  NULL;
+}
