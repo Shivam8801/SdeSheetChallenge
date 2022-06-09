ea2cb686258c4bd3438561466a2f75b420cbd804
Shivam Nikam <shivammh15@gmail.com>
2022-06-09 21:28:23 +0530

    Create mergeTwoSortedLists.cpp

---
+++ Day5/mergeTwoSortedLists.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h> 
+using namespace std;
+/************************************************************
+
+    Following is the linked list node structure.
+    
+    template <typename T>
+    class Node {
+        public:
+        T data;
+        Node* next;
+
+        Node(T data) {
+            next = NULL;
+            this->data = data;
+        }
+
+        ~Node() {
+            if (next != NULL) {
+                delete next;
+            }
+        }
+    };
+
+************************************************************/
+
+Node<int>* sortTwoLists(Node<int>* first, Node<int>* second)
+{
+    if(first == NULL) return second;
+    if(second == NULL) return first;
+    if(first == NULL && second == NULL) return NULL;
+    
+    if(first->data > second->data) swap(first, second);
+    Node<int>* res = first;
+    
+    while(first != NULL && second != NULL)
+    {
+        Node<int>* temp;
+        while(first != NULL && first->data <= second->data)
+        {
+            temp = first;
+            first = first->next;
+        }
+        temp->next = second;
+        swap(first, second);
+    }
+    return res; 
+}
