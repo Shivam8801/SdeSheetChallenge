1557671aa581869a10c8c987cb6153c4e70ad72e
Shivam Nikam <shivammh15@gmail.com>
2022-07-23 17:05:22 +0530

    Create M-Coloring.cpp

---
+++ Day10/M-Coloring.cpp
@@ -0,0 +1,32 @@
+bool isValid(vector<vector<int>> &mat, vector<int> &colour, int c, int node, int sz)
+{
+    for(int i = 0; i < mat.size(); i++)
+    {
+        if(i != node && mat[node][i] == 1 && colour[i] == c)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool solver(vector<vector<int>> &mat, vector<int> &colour, int m, int row, int sz)
+{
+    if(row == sz) return true;
+    for(int c = 1; c <= m; c++)
+    {
+        if(isValid(mat, colour, c, row, sz))
+        {
+            colour[row] = c;
+            if(solver(mat, colour, m, row + 1, sz)) return true;
+            colour[row] = 0;
+        }
+    }
+    return false;
+}
+
+string graphColoring(vector<vector<int>> &mat, int m) {
+    int sz = mat.size();
+    vector<int> colour(sz, 0);
+    return (solver(mat, colour, m, 0, sz)) ? "YES" : "NO";
+}
