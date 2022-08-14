660e811ddf1cacbf8dfb47ea6e48555131bf90f2
Shivam Nikam <shivammh15@gmail.com>
2022-08-14 15:04:52 +0530

    Create SearchInRotated.cpp

---
+++ Day11/SearchInRotated.cpp
@@ -0,0 +1,35 @@
+int search(int* arr, int n, int key) {
+    int low = 0, high = n - 1;
+    
+    while(low <= high)
+    {
+        int mid = (low + high) >> 1;
+        if(arr[mid] == key) return mid;
+
+        if(arr[mid] >= arr[low])
+        {
+            if(arr[mid] >= key && arr[low] <= key)
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+
+        else
+        {
+            if(arr[mid] <= key && arr[high] >= key)
+            {
+
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+    }
+    return -1;
+}
